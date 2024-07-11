6a6606ad0caad38f71b2de70bd64ff74880526de
hos8 <jam39so@outlook.com>
2024-07-11 13:03:30 +0800

    Modify prepare.c

--- prepare.c
+++ prepare.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Draw bad measure most power whom discussion.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
