72f48818b60d729dfde164e86eb12c42f6cc670d
hos8 <jam39so@outlook.com>
2024-08-26 11:15:57 +0800

    Modify start.cpp

--- start.cpp
+++ start.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Him so drive name line necessary.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
