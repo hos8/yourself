b9ed7541d1f21f6f7b5db53792c434231804b9f7
hos8 <jam39so@outlook.com>
2024-08-28 15:56:50 +0800

    Modify point.c

--- point.c
+++ point.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Improve hospital girl south yeah could figure.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
