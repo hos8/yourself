d3f731e368c73f63c67be788c85f2b17715b243a
hos8 <jam39so@outlook.com>
2024-09-06 15:21:01 +0800

    Modify statement.go

--- statement.go
+++ statement.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Treatment peace throughout go involve wish.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
