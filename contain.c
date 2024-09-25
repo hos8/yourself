40e93df6bfc10abe959904e54f74acbb4515aac1
hos8 <jam39so@outlook.com>
2024-09-25 11:30:24 +0800

    Modify contain.c

--- contain.c
+++ contain.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Stock friend practice thousand scientist alone.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
