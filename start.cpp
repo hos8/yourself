7f161d56b0313e651cb69bccab08f187089361a9
hos8 <jam39so@outlook.com>
2024-10-08 17:31:51 +0800

    Modify start.cpp

--- start.cpp
+++ start.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Return guess simply.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
