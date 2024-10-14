0c8690b75e0a0dc981a242613e9987649a2d2b87
hos8 <jam39so@outlook.com>
2024-10-14 10:31:28 +0800

    Modify artist.cpp

--- artist.cpp
+++ artist.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Choose treat TV avoid two.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
