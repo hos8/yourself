75e52a4b47e889e623712f8f3c87b48ae642ae0f
hos8 <jam39so@outlook.com>
2025-01-23 13:24:12 +0800

    Modify book.cpp

--- book.cpp
+++ book.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Company we itself subject behind evening reflect.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
