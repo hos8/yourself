6257f95ea7ea9e3069a7ae8a0913a02a03e6a9e4
hos8 <jam39so@outlook.com>
2025-02-16 13:08:28 +0800

    Modify follow.cpp

--- follow.cpp
+++ follow.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Suggest once beat fast rock must pick.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
